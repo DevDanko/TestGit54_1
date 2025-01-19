6abaaefc9dd64af90ed871cff8168ad07e25dd8f
Eugen <eg31it@gmail.com>
2025-01-19 21:04:02 +0100

    Add service

---
+++ src/services/BookServices.java
@@ -0,0 +1,16 @@
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(Book book) {
+        bookRepository.addBook(book);
+    }
+
+    public ArrayList<Book> getAllBooks() {
+        return bookRepository.getAllBooks();
+    }
+}
